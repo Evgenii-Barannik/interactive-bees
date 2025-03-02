8b304cbae335237f50c4e30a8b075a84903b111a
Evgenii-Barannik <radchemsu@gmail.com>
2025-03-02 04:16:52 +0200

    minor

--- main.py
+++ main.py
@@ -34,24 +34,19 @@ if __name__ == "__main__":
 
     time_slider_html = plot_time_slider(filtered_dataset)
     acoustic_spectra_html = plot_acoustic_spectra(filtered_dataset, start, end)
-    temperature_humidity_html = plot_temperature_humidity(filtered_dataset)
     parallel_selector_html = plot_parallel_selector(filtered_dataset)
     _ = plot_similarity(dataset, start, end, OUTPUT_DIR)
     _ = plot_gaussians(dataset, start, end, OUTPUT_DIR)
 
     with open(ACOUSTIC_SPECTRA_INFO, "r") as f:
         acoustic_spectra_info = f.read()
-    with open(TEMPERATURE_HUMIDIY_INFO, "r") as f:
-        temperature_humidity_info = f.read()
     with open(SIMILARITY_INFO, "r") as f:
         similarity_info = f.read()
 
     html_data = {
-        "time_slider_plot": time_slider_html,
         "acoustic_spectra_plot" : acoustic_spectra_html,
         "acoustic_spectra_info" : acoustic_spectra_info,
-        "temperature_humidity_plot": temperature_humidity_html,
-        "temperature_humidity_info": temperature_humidity_info,
+        "time_slider_plot": time_slider_html,
         "parallel_selector_plot": parallel_selector_html,
         "similarity_info": similarity_info,
         "sensors": sensors,
